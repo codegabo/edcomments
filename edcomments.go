6ed81cef8ce412565eb44d3a1d374c5c9a61f2b0
ggabooo <ggabooo@hotmail.com>
2017-06-10 15:13:04 -0500

    Rutas y compilación
    compilación de edcomments.go y ejecución de edcomments.exe

--- edcomments.go
+++ edcomments.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 
 	"github.com/golang-es/edcomments/migration"
+	"github.com/golang-es/edcomments/routes"
+	"github.com/urfave/negroni"
 )
 
 func main() {
@@ -17,4 +20,21 @@ func main() {
 		migration.Migrate()
 		log.Println("Ha finalizado la migración")
 	}
+
+	// Inicia la ruta
+	router := routes.InitRoutes()
+
+	// Inicia los middleWares
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	// Inicia el servidor
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: n,
+	}
+
+	log.Println("Iniciado el servidor en http://localhost:8080")
+	log.Println(server.ListenAndServe())
+	log.Println("ha finalizado la ejecucion del programa")
 }
