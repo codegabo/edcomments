149e4da6e0ab2ad913a196aec590cd30b40a5ccf
ggabooo <ggabooo@hotmail.com>
2017-06-10 20:12:35 -0500

    correcciones, compilación y pruebas en postman
    correcciones de errors de typo en routes/comment.go y compilacion, después de realizan pruebas con postman  creando comentarios y comentarios hijos

--- routes/comment.go
+++ routes/comment.go
@@ -1,20 +1,21 @@
 package routes
 
 import (
-  "github.com/gorilla/mux"
-  "github.com/urfave/negroni"
-  "github.com/golang-es/edcomments/controllers"
+	"github.com/golang-es/edcomments/controllers"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
-// si el token es valido se ejecutara el SubRouter
- func SetCommentRouter(router *mux.Router) {
-   prefix "/api/comments"
-   subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter(.StrictSlash)(true)
-   SubRouter.HandleFunc("/", controllers.CommentsCreate).Methods("POST")
 
-   router.PathPrefix(prefix).Handler(
-     negroni.New(
-       negroni.HandlerFunc(controllers.VakidateToken),
-       negroni.Wrap(SupRouter),
-     )
-   )
- }
+// si el token es valido se ejecutara el SubRouter
+func SetCommentRouter(router *mux.Router) {
+	prefix := "/api/comments"
+	subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter().StrictSlash(true)
+	subRouter.HandleFunc("/", controllers.CommentCreate).Methods("POST")
+
+	router.PathPrefix(prefix).Handler(
+		negroni.New(
+			negroni.HandlerFunc(controllers.ValidateToken),
+			negroni.Wrap(subRouter),
+		),
+	)
+}
