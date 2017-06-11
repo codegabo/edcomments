5b82a88824c13d3839ae6c5f92aa0edb8c31a9e0
ggabooo <ggabooo@hotmail.com>
2017-06-11 11:17:39 -0500

    Consulta comentarios
    crear controlador para consultar los comentarios que no tienen comentaarios hijos, y agregar dicha función que controla los comentarios en el router. Actualización prcedimiento

--- controllers/comment.go
+++ controllers/comment.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/golang-es/edcomments/commons"
 	"github.com/golang-es/edcomments/configuration"
@@ -38,4 +40,56 @@ func CommentCreate(w http.ResponseWriter, r *http.Request) {
 	m.Message = "Comentario creado con extitó"
 	commons.DisplayMessage(w, m)
 
+}
+
+//FUNCIONALIDAD PARA OBTENER TODOS LOS COMENTARIOS
+// CommentGetAll obtinene todos los comentarios
+
+func CommentGetAll(w http.ResponseWriter, r *http.Request) {
+	comments := []models.Comment{}
+	m := models.Message{}
+	user := models.User{}
+	//vote := models.Vote{}
+
+	// esto para saber que usuario esta realizando la consulta
+	r.Context().Value(&user)
+	vars := r.URL.Query()
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	// se prepara la consulta
+	cComment := db.Where("parent_id = 0")
+	if order, ok := vars["order"]; ok {
+		if order[0] == "votes" {
+			cComment = cComment.Order("votes desc, created_at desc")
+		}
+	} else {
+		if idlimit, ok := vars["idlimit"]; ok {
+			registerByPage := 30
+			offset, err := strconv.Atoi(idlimit[0])
+			if err != nil {
+				log.Println("Error:", err)
+			}
+			cComment = cComment.Where("id BETWEEN ? AND ?", offset-registerByPage, offset)
+		}
+		cComment = cComment.Order("id desc")
+	}
+	cComment.Find(&comments)
+	j, err := json.Marshal(comments)
+	if err != nil {
+		m.Code = http.StatusInternalServerError
+		m.Message = "Error al convertir los comentarios en json"
+		commons.DisplayMessage(w, m)
+		return
+	}
+	if len(comments) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m.Code = http.StatusNoContent
+		m.Message = "No se encontraron comentarios"
+		commons.DisplayMessage(w, m)
+	}
 }
